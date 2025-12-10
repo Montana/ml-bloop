d1c22fab30e8cbec62698362641277519776f891
Michael Mendy <montana@linux.com>
2025-12-09 16:20:13 -0800

    rescaling now properly normalizes by the sum of weights when rescale is `true`.

--- src/bloop/optim.py
+++ src/bloop/optim.py
@@ -2,7 +2,7 @@
 # For licensing see accompanying LICENSE file.
 # Copyright (C) 2025 Apple Inc. All Rights Reserved.
 #
-from typing import Any, Literal, Optional
+from typing import any, literal, optional
 
 import jax
 import jax.numpy as jnp
@@ -13,8 +13,8 @@ from flax.core import frozen_dict
 from bloop.utils import random_init_ema, tree_proj, update_ema
 
 __all__ = [
-    "BloopState",
-    "PCGradState",
+    "bloopstate",
+    "pcgradstate",
     "init_bloop",
     "init_pcgrad",
     "bloop_direction",
@@ -23,12 +23,12 @@ __all__ = [
     "mixed_direction",
 ]
 
-Gradients = frozen_dict.FrozenDict[str, Any]
+gradients = frozen_dict.frozendict[str, any]
 
 
 @struct.dataclass
-class BloopState:
-    grad_main_ema: Gradients
+class bloopstate:
+    grad_main_ema: gradients
     ema: float = 0.1
     lbda: float = 0.1
     rescale: bool = struct.field(default=False, pytree_node=False)
@@ -36,24 +36,24 @@ class BloopState:
 
 
 @struct.dataclass
-class PCGradState:
-    grad_main_ema: Gradients
-    grad_aux_ema: Gradients
+class pcgradstate:
+    grad_main_ema: gradients
+    grad_aux_ema: gradients
     ema: float = 0.1
     lbda: float = 0.1
     rescale: bool = struct.field(default=False, pytree_node=False)
 
 
 def init_bloop(
-    key: jax.Array,
-    grad_main: Gradients,
+    key: jax.array,
+    grad_main: gradients,
     ema: float = 0.1,
     lbda: float = 0.1,
     *,
-    init: Literal["grad", "zeros", "random"] = "grad",
+    init: literal["grad", "zeros", "random"] = "grad",
     rescale: bool = False,
     use_ratio_only: bool = False,
-) -> BloopState:
+) -> bloopstate:
     if init == "grad":
         grad_main_ema = jtu.tree_map(lambda x: jnp.copy(x), grad_main)
     elif init == "zeros":
@@ -61,12 +61,12 @@ def init_bloop(
     elif init == "random":
         grad_main_ema = random_init_ema(key, grad_main)
     else:
-        raise ValueError(
-            f"Invalid init value {init!r}. Valid options are 'grad', 'zeros' or 'random'."
+        raise valueerror(
+            f"invalid init value {init!r}. valid options are 'grad', 'zeros' or 'random'."
         )
 
-    return BloopState(
-        grad_main_ema,
+    return bloopstate(
+        grad_main_ema=grad_main_ema,
         ema=ema,
         lbda=lbda,
         rescale=rescale,
@@ -75,15 +75,16 @@ def init_bloop(
 
 
 def init_pcgrad(
-    key: jax.Array,
-    grad_main: Gradients,
+    key: jax.array,
+    grad_main: gradients,
     ema: float = 0.1,
     lbda: float = 0.1,
     *,
-    init: Literal["grad", "zeros", "random"] = "grad",
+    init: literal["grad", "zeros", "random"] = "grad",
     rescale: bool = False,
-) -> PCGradState:
+) -> pcgradstate:
     main_init_key, aux_init_key = jax.random.split(key, 2)
+
     bloop_params = init_bloop(
         main_init_key,
         grad_main,
@@ -92,12 +93,13 @@ def init_pcgrad(
         init=init,
         rescale=rescale,
     )
+
     if init == "random":
         grad_aux_ema = random_init_ema(aux_init_key, grad_main)
     else:
         grad_aux_ema = bloop_params.grad_main_ema
 
-    return PCGradState(
+    return pcgradstate(
         grad_main_ema=bloop_params.grad_main_ema,
         grad_aux_ema=grad_aux_ema,
         ema=bloop_params.ema,
@@ -107,19 +109,23 @@ def init_pcgrad(
 
 
 def _bloop_direction_general(
-    grad_main: Gradients,
-    grad_aux: Gradients,
-    bloop_state: BloopState,
-    ratio_upper_bound: Optional[float] = None,
+    grad_main: gradients,
+    grad_aux: gradients,
+    bloop_state: bloopstate,
+    ratio_upper_bound: optional[float] = None,
     eps: float = 1e-7,
-) -> tuple[Gradients, BloopState]:
+) -> tuple[gradients, bloopstate]:
     grad_main_ema = update_ema(grad_main, bloop_state.ema, bloop_state.grad_main_ema)
     lbda = bloop_state.lbda
-    scale = (1.0 / 1.0 + lbda) if bloop_state.rescale else 1.0
+    scale = 1.0 / (1.0 + lbda) if bloop_state.rescale else 1.0
 
     projected_bias, main_grad_add_factor = tree_proj(
-        grad_main_ema, grad_aux, ratio_upper_bound=ratio_upper_bound, eps=eps
+        grad_main_ema,
+        grad_aux,
+        ratio_upper_bound=ratio_upper_bound,
+        eps=eps,
     )
+
     if bloop_state.use_ratio_only:
         direction = jtu.tree_map(
             lambda a, b: scale * ((1.0 + lbda * main_grad_add_factor) * a + lbda * b),
@@ -127,7 +133,11 @@ def _bloop_direction_general(
             grad_aux,
         )
     else:
-        direction = jtu.tree_map(lambda a, b: scale * (a + lbda * b), grad_main, projected_bias)
+        direction = jtu.tree_map(
+            lambda a, b: scale * (a + lbda * b),
+            grad_main,
+            projected_bias,
+        )
 
     bloop_state = bloop_state.replace(
         grad_main_ema=grad_main_ema,
@@ -136,11 +146,11 @@ def _bloop_direction_general(
 
 
 def bloop_direction(
-    grad_main: Gradients,
-    grad_aux: Gradients,
-    bloop_state: BloopState,
+    grad_main: gradients,
+    grad_aux: gradients,
+    bloop_state: bloopstate,
     eps: float = 1e-7,
-) -> tuple[Gradients, BloopState]:
+) -> tuple[gradients, bloopstate]:
     return _bloop_direction_general(
         grad_main,
         grad_aux,
@@ -151,11 +161,11 @@ def bloop_direction(
 
 
 def dynamic_barrier_direction(
-    grad_main: Gradients,
-    grad_aux: Gradients,
-    bloop_state: BloopState,
+    grad_main: gradients,
+    grad_aux: gradients,
+    bloop_state: bloopstate,
     eps: float = 1e-7,
-) -> tuple[Gradients, BloopState]:
+) -> tuple[gradients, bloopstate]:
     return _bloop_direction_general(
         grad_main,
         grad_aux,
@@ -166,31 +176,40 @@ def dynamic_barrier_direction(
 
 
 def pcgrad_direction(
-    grad_main: Gradients,
-    grad_aux: Gradients,
-    pcgrad_state: PCGradState,
+    grad_main: gradients,
+    grad_aux: gradients,
+    pcgrad_state: pcgradstate,
     eps: float = 1e-7,
-) -> tuple[Gradients, PCGradState]:
-    """The original version of pcgrad!
+) -> tuple[gradients, pcgradstate]:
+    grad_main_ema = update_ema(
+        grad_main, pcgrad_state.ema, pcgrad_state.grad_main_ema
+    )
+    grad_aux_ema = update_ema(
+        grad_aux, pcgrad_state.ema, pcgrad_state.grad_aux_ema
+    )
 
-    From https://arxiv.org/abs/2001.06782
-    """
-    # update EMA
-    grad_main_ema = update_ema(grad_main, pcgrad_state.ema, pcgrad_state.grad_main_ema)
-    grad_aux_ema = update_ema(grad_aux, pcgrad_state.ema, pcgrad_state.grad_aux_ema)
-    # projecting
-    ratio_upper_bound = 0
-    projected_bias, train_ema_factor = tree_proj(
-        grad_main_ema, grad_aux, ratio_upper_bound=ratio_upper_bound, eps=eps
+    ratio_upper_bound = 0.0
+    projected_bias, _train_ema_factor = tree_proj(
+        grad_main_ema,
+        grad_aux,
+        ratio_upper_bound=ratio_upper_bound,
+        eps=eps,
     )
     projected_train, _ = tree_proj(
-        grad_aux_ema, grad_main, ratio_upper_bound=ratio_upper_bound, eps=eps
+        grad_aux_ema,
+        grad_main,
+        ratio_upper_bound=ratio_upper_bound,
+        eps=eps,
     )
-    # compute direction
+
     lbda = pcgrad_state.lbda
     scale = 1.0 / (1.0 + lbda) if pcgrad_state.rescale else 1.0
+    direction = jtu.tree_map(
+        lambda a, b: scale * (a + lbda * b),
+        projected_train,
+        projected_bias,
+    )
 
-    direction = jtu.tree_map(lambda a, b: scale * (a + lbda * b), projected_train, projected_bias)
     pcgrad_state = pcgrad_state.replace(
         grad_main_ema=grad_main_ema,
         grad_aux_ema=grad_aux_ema,
@@ -199,12 +218,15 @@ def pcgrad_direction(
 
 
 def mixed_direction(
-    grad_main: Gradients,
-    grad_aux: Gradients,
-    bloop_state: BloopState,
-) -> tuple[Gradients, BloopState]:
+    grad_main: gradients,
+    grad_aux: gradients,
+    bloop_state: bloopstate,
+) -> tuple[gradients, bloopstate]:
     lbda = bloop_state.lbda
     scale = 1.0 / (1.0 + lbda) if bloop_state.rescale else 1.0
-
-    direction = jtu.tree_map(lambda a, b: scale * (a + lbda * b), grad_main, grad_aux)
+    direction = jtu.tree_map(
+        lambda a, b: scale * (a + lbda * b),
+        grad_main,
+        grad_aux,
+    )
     return direction, bloop_state
